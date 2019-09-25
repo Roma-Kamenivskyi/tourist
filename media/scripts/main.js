6009a9e93d433c974714e476abb0015264453233
Roma Kamenivskiy <jumji19992412@gmail.com>
2019-09-25 15:08:03 +0300

    test

--- media/scripts/main.js
+++ media/scripts/main.js
@@ -7,11 +7,6 @@ $(function() {
     accessibility: true,
     infinite: true,
     dots: false,
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> 1f1ab21db7fba07d0ec33ac28dd5b888185c38a8
     responsive: [
       {
         breakpoint: 1366,
@@ -45,39 +40,23 @@ $(function() {
       }
     ]
   });
-  
-  $('.slick-cards').slick({
-    centerMode: true,
-    slidesToShow: 4,
-    infinite: true,
 
-<<<<<<< HEAD
   $(".slick-cards").slick({
     centerMode: true,
     slidesToShow: 4,
     infinite: true,
 
-=======
->>>>>>> 1f1ab21db7fba07d0ec33ac28dd5b888185c38a8
     responsive: [
       {
         breakpoint: 1100,
         settings: {
-<<<<<<< HEAD
           slidesToShow: 3
-=======
-          slidesToShow: 3,
->>>>>>> 1f1ab21db7fba07d0ec33ac28dd5b888185c38a8
         }
       },
       {
         breakpoint: 800,
         settings: {
-<<<<<<< HEAD
           slidesToShow: 2
-=======
-          slidesToShow: 2,
->>>>>>> 1f1ab21db7fba07d0ec33ac28dd5b888185c38a8
         }
       },
       {
@@ -96,11 +75,7 @@ $(function() {
       }
     ]
   });
-<<<<<<< HEAD
   $(".partners-slick").slick({
-=======
-  $('.partners-slick').slick({
->>>>>>> 1f1ab21db7fba07d0ec33ac28dd5b888185c38a8
     infinite: true,
     slidesToShow: 5,
     slidesToScroll: 2,
@@ -109,11 +84,7 @@ $(function() {
       {
         breakpoint: 800,
         settings: {
-<<<<<<< HEAD
           slidesToShow: 4
-=======
-          slidesToShow: 4,
->>>>>>> 1f1ab21db7fba07d0ec33ac28dd5b888185c38a8
         }
       },
       {
@@ -133,21 +104,13 @@ $(function() {
       {
         breakpoint: 350,
         settings: {
-<<<<<<< HEAD
           slidesToShow: 1
-=======
-          slidesToShow: 1,
->>>>>>> 1f1ab21db7fba07d0ec33ac28dd5b888185c38a8
         }
       }
     ]
   });
 
-<<<<<<< HEAD
   // Navbar accordion dropdown
-=======
-  // Navbar dropdown
->>>>>>> 1f1ab21db7fba07d0ec33ac28dd5b888185c38a8
   $(".dropdown-toggle").on("click", function() {
     $(".dropdown-menu")
       .not($(this).next())
@@ -158,8 +121,8 @@ $(function() {
   });
 
   // Mobile burger menu
-<<<<<<< HEAD
   const mobileWrap = $(".mobile-nav-wrapper");
+
   $(".burger-menu").on("click", function() {
     mobileWrap.css({
       left: "0",
@@ -174,17 +137,4 @@ $(function() {
       right: "auto"
     });
   });
-=======
-  let mobileWrap = $('.mobile-nav-wrapper');
-  $(".burger-menu").on("click", function() {
-    mobileWrap.slideDown();
-    $("html, body").css('overflow', 'hidden');
-  });
-
-  $('.close-mobile-nav').on('click', function() {
-    mobileWrap.slideUp();
-    $("html, body").css('overflow', 'visible');
-  });
-
->>>>>>> 1f1ab21db7fba07d0ec33ac28dd5b888185c38a8
 });
