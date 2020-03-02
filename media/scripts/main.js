ba62109a098921fa657076b12f1757ea823c699a
Roman Kamenivskyi <jumji19992412@gmail.com>
2020-03-03 00:03:54 +0200

    fixed small bugs related with convenience carousel

--- media/scripts/main.js
+++ media/scripts/main.js
@@ -1,18 +1,18 @@
 $(function() {
   // Global variables
-  const toTopBtn = $("#toTopBtn");
-  const mobileWrap = $(".mobile-nav-wrapper");
+  const toTopBtn = $('#toTopBtn');
+  const mobileWrap = $('.mobile-nav-wrapper');
 
   // Event Listeners
-  $(".close-mobile-nav").on("click", closeMobileNav);
-  $(".burger-menu").on("click", openMobileNav);
-  $(".dropdown-toggle").on("click", toggleDropdownAccordion);
-  $(window).on("scroll", displayBtnByScroll);
-  toTopBtn.on("click", scrollToTop);
+  $('.close-mobile-nav').on('click', closeMobileNav);
+  $('.burger-menu').on('click', openMobileNav);
+  $('.dropdown-toggle').on('click', toggleDropdownAccordion);
+  $(window).on('scroll', displayBtnByScroll);
+  toTopBtn.on('click', scrollToTop);
 
   // functions
   function toggleDropdownAccordion() {
-    $(".dropdown-menu")
+    $('.dropdown-menu')
       .not($(this).next())
       .slideUp();
     $(this)
@@ -20,39 +20,34 @@ $(function() {
       .slideToggle();
   }
   function openMobileNav() {
-    $("html, body").css("overflow-y", "hidden");
+    $('html, body').css('overflow-y', 'hidden');
     mobileWrap.css({
-      transform: "translateX(0%)"
+      transform: 'translateX(0%)'
     });
   }
   function closeMobileNav() {
-    $("html, body").css("overflow-y", "auto");
+    $('html, body').css('overflow-y', 'auto');
     mobileWrap.css({
-      transform: "translateX(-100%)"
+      transform: 'translateX(-100%)'
     });
   }
 
   function displayBtnByScroll() {
     if ($(this).scrollTop() > 400) {
-      toTopBtn.css("bottom", "6rem");
+      toTopBtn.css('bottom', '6rem');
     } else {
-      toTopBtn.css("bottom", "-10rem");
+      toTopBtn.css('bottom', '-10rem');
     }
   }
 
   function scrollToTop(event) {
     event.preventDefault();
-    $("body, html").animate({ scrollTop: 0 }, 500);
+    $('body, html').animate({ scrollTop: 0 }, 500);
   }
 
   // Plugins init
-  const typed = new Typed("#typed", {
-    strings: [
-      "We know what you need",
-      "Join us, you will not regret",
-      "Save Upto 40% For Upcoming Summer",
-      "We have very nice prices, check it out"
-    ],
+  const typed = new Typed('#typed', {
+    strings: ['We know what you need', 'Join us, you will not regret'],
     startDelay: 20,
     backSpeed: 40,
     typeSpeed: 70,
@@ -60,7 +55,7 @@ $(function() {
   });
 
   // Slick carousel
-  $(".slick").slick({
+  $('.slick').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: false,
@@ -102,7 +97,7 @@ $(function() {
     ]
   });
 
-  $(".slick-cards").slick({
+  $('.slick-cards').slick({
     centerMode: true,
     slidesToShow: 4,
     infinite: true,
@@ -136,7 +131,7 @@ $(function() {
       }
     ]
   });
-  $(".partners-slick").slick({
+  $('.partners-slick').slick({
     infinite: true,
     slidesToShow: 5,
     slidesToScroll: 2,
@@ -165,7 +160,8 @@ $(function() {
       {
         breakpoint: 350,
         settings: {
-          slidesToShow: 1
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       }
     ]
